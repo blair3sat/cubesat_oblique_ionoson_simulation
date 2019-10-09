4f6c781953e2e51770342c2f64f8d57d78eca56e
jaxon-blair3sat <55414684+jaxon-blair3sat@users.noreply.github.com>
2019-10-08 23:15:29 -0400

    Make code more readable and finish logic

--- sim1.py
+++ sim1.py
@@ -1,4 +1,6 @@
 import math
+import time
+import sys
 
 def phi(time):
     return 100000
@@ -6,49 +8,65 @@ def phi(time):
 def freqFromTime(t, dist):
     return freq(t-dist/c)
 
-def inBounds1(time):
-    return sX<=UpperBound1 and sX>=LowerBound1
+def isInBoundsInitial():
+    return satXPos <= initialSatEntrancePoint and satXPos >= initialSatExitPoint
 
-def inBounds2(time):
-    return sX<=UpperBound2 and sX>=LowerBound2
+def isInBoundsSecond():
+    return satXPos <= secondSatEntrancePoint and satXPos >= secondSatExitPoint
 
-t = 0
+def shouldContinueSimulation():
+    return(satXpos >= secondSatExitPoint)
+
+time = 0
 tStep = 0.1
-xVeloc = 1
-c = 300000000 #meters per second
-satYPos = 290
-x1 = 290
-y1 = 290
-x2 = 290
-y2 = 290
+# Velocity in m / tstep
+xVeloc = 100
+c = 300000000 # meters per second (speed of light in vacuum)
+satYPos = 100
+satXPos = 0
+# First boundry
 distance = 290
-theta = 290
-beta = 290
-height = 290
-freq = 0
-LowerBound1 = (sY-y1)/math.tan(theta+beta)+x1
-UpperBound1 = (sY-y1)/math.tan(beta)+x1
-sX = (sY-y1)/math.tan(theta+beta)+x1
+# Degrees from the horizontal
+lowerAngle = 10
+upperAngle = 20
+# Height that a given radio wave bounces off the ionosphere. Changes based on frequency.
+heightOfWaveReflection = 2352345245435
+initialFreq = 0
+freq = initialFrequency
+# Hz / second
+chirpRate = 100000
+
+# Figuring out upper and lower bounds of the ionosonde radio wave. Variable names confusing?
+initialSatEntrancePoint = satYPos / tan(upperAngle)   # Leftmost point of non-reflected radio wave
+initialSatExitPoint = satYPos / tan(lowerAngle)   # Rightmost point of non-reflected radio wave
+finalSatEntrancePoint = (height - satYPos) / tan(upperAngle) # sat enters the area where it would be affected by the wave
+finalSatExitPoint = (height - satYPos) / tan(lowerAngle)
+
+
+frequencyToHeightConversionRate = 475 / 7000
 dataArr = []
 while True:
-    t += tStep
-    sX += xVeloc
-    LowerBound2 = (2*h-sY-y1)/math.tan(theta+beta)+x1
-    UpperBound2 = (2*h-sY-y1)/math.tan(beta)+x1
-
-    freq += phi(t) * tStep
-
-    if inBounds1(t) and inBounds2(t):
-        pass
-    elif inBounds1(t):
-        dataArr.append([t, freq])
-    elif inBounds2(t):
-        #TODO: figure out how to calculate the frequency after it has reflected
+    initialFreq += chirpRate * 0.1
+    time += tStep
+    # The x component of the Sat is increased by its velocity every tstep
+    satXPos += xVeloc
+    heightOfWaveReflection = time * c / 2 # This isnt right yet
+    secondSatEntrancePoint = (heightOfWaveReflection - satYPos) / tan(upperAngle)
+    secondSatExitPoint = (heightOfWaveReflection - satYPos) / tan(lowerAngle)
+    freq = time * chirpRate
+    if inBoundsInitial():
+        dataArr.append([time, freq])
+    elif inBoundsSecond():
+        dataArr.append([time,freq])    #TODO: figure out how to calculate the frequency after it has reflected
+    elif inBoundsInitial() and inBoundsSecond():
+        print "we don't know how to calculate what happens when the Sat is in this zone yet"
+        sys.exit(2)
+        # TODO figure out how to calculate frequency in this area
     else:
         pass
-
-    #there was an empty todo here so rewrite the todo if you see this
-    if noLongerNeedToSimulate:
+        # Not sensing anything
+    # there was an empty todo here so rewrite the todo if you see this
+    if shouldExitSimulation():
         break
 
 #TODO: Output the data to a text file and a graph
